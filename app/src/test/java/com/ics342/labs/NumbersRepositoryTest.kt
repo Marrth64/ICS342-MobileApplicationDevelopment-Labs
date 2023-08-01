e069b568e56ac33f9cf66b1e83f8031abc1ee0aa
Matthew Davis <marrthistyping64@gmail.com>
2023-07-31 21:44:52 -0500

    finished lab9

--- app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
+++ app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
@@ -3,18 +3,16 @@ package com.ics342.labs
 import android.os.Parcel
 import android.os.Parcelable
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import java.util.UUID
 import kotlin.random.Random
 
-internal class NumbersRepositoryTest() : Parcelable {
-
-    constructor(parcel: Parcel) : this() {
-    }
-
+internal class NumbersRepositoryTest {
     @Test
     fun `If database does not have a number fetch it from the Api`() {
         // Setup
@@ -30,6 +28,7 @@ internal class NumbersRepositoryTest() : Parcelable {
         val repository = NumbersRepository(database, api)
         val result = repository.getNumber(id)
 
+
         // Assert
         assertNotNull(result)
         assertEquals(result, number)
@@ -37,27 +36,27 @@ internal class NumbersRepositoryTest() : Parcelable {
         verify { database.getNumber(id) }
         verify { api.getNumber(id) }
     }
-
     @Test
-    fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
-        // TODO: implement this test
-    }
+    fun `ifDatabaseIsEmptyShouldFetchNumbersFromApi`() {
+        // Setup
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val number = Number(UUID.randomUUID().toString(), Random.nextInt())
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        every{ database.storeNumbers(listOf(number)) } just runs
+        every { database.getAllNumbers() } returns emptyList()
+        every { api.getNumbers() } returns listOf(number)
 
-    }
+        // Act
+        val repository = NumbersRepository(database, api)
+        val result = repository.fetchNumbers()
 
-    override fun describeContents(): Int {
-        return 0
-    }
+        // Assert
+        assertNotNull(result)
+        assertEquals(result, listOf(number))
 
-    companion object CREATOR : Parcelable.Creator<NumbersRepositoryTest> {
-        override fun createFromParcel(parcel: Parcel): NumbersRepositoryTest {
-            return NumbersRepositoryTest(parcel)
-        }
+        verify { database.getAllNumbers()}
+        verify { api.getNumbers() }
 
-        override fun newArray(size: Int): Array<NumbersRepositoryTest?> {
-            return arrayOfNulls(size)
-        }
     }
 }
